8a9257627493a5437a62d2440dfdb9fc853f713e
Samuel Shen <sshen05@terpmail.umd.edu>
2024-03-07 10:34:37 -0500

    Finish ex5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailVisible, setDetailVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const pStyle = {
+    marginBlock: 2,
+  }
+
+  const showWhenVisible = {
+    display: detailVisible ? '' : 'none',
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setDetailVisible(!detailVisible)}>{detailVisible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p style={pStyle}>{blog.url}</p>
+        <p style={pStyle}>likes {blog.likes} <button>like</button></p>
+        <p style={pStyle}>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
